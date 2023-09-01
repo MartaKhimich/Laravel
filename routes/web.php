78f9d085aa738c7a8c7ed9102826c0a26f8517d3
martakhimich <marta_himich@mail.ru>
2023-09-02 01:13:04 +0300

    new pages

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//страница приветствия пользователей
 Route::get('/hello/{name}', static function(string $name): string {
     return "Hello, {$name}";
 });
 
+//страница с информацией о проекте
 Route::get('/info', function() {
     return "Project GeekBrains. Student: Marta Khimich";
 });
 
+//страница для вывода нескольких новостей
 Route::get('/news', function() {
     return "News";
 });
 
+//страница для вывода одной новости
 Route::get('/news/{id}', static function(int $id): string {
     return "News with #ID {$id}";
 });
